e628cde605ebedcd5de5f321480ba69380faa162
FinSec Admin <admin@finsec.systems>
2021-11-15 10:29:56 -0800

    Updated dependencies, logging

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
-../scripts/deploy.sh
+#!/bin/bash
+pushd . > /dev/null
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $dir
+./deploy-script.sh nopush $@
+popd > /dev/null
